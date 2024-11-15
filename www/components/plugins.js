926897e4701328bd37155cae84bc10e6571c2d24
isabelamarchesoni <isamarchesoniserra@gmail.com>
2024-11-15 10:23:44 +0900

    Finalização

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,19 @@
-// This is a JavaScript file
+$(document).on("click","#camera",function(){
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI 
+    correctOrientation: true,
+    saveToPhotoAlbum: true  
+});
+
+function onSuccess(imageURI) {
+        var img = document.getElementById('myImagem');
+        image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+});
+
+
 
